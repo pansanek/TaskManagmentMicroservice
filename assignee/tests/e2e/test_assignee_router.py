cfc5c0aee30daaa52ed4ed668524bf4b07af334c
Александр Потемкин <90202010+pansanek@users.noreply.github.com>
2023-12-08 01:15:48 +0300

    Update test_assignee_router.py

--- assignee/tests/e2e/test_assignee_router.py
+++ assignee/tests/e2e/test_assignee_router.py
@@ -10,65 +10,42 @@ time.sleep(5)
 base_url = 'http://localhost:8000/api/assignees'
 
 @pytest.fixture(scope='session')
-def first_assignee_data() -> dict:
-    return {
-        'name': 'Assignee 1',
-    }
+def first_assignee_data() -> Assignee:
+    return Assignee(id=10,name='Name',taskcount=0)
 
 @pytest.fixture(scope='session')
-def second_assignee_data() -> dict:
-    return {
-        'name': 'Assignee 2',
-    }
+def second_assignee_data() -> Assignee:
+    return Assignee(id=12,name='Namee',taskcount=0)
 
-def test_get_assignees_empty() -> None:
+def test_get_assignees_created_in_repo() -> None:
     assert requests.get(f'{base_url}/').json() == []
 
 def test_create_assignee_first_success(
-        first_assignee_data: dict
+        first_assignee_data: Assignee
 ) -> None:
-    response = requests.post(f'{base_url}/', json=first_assignee_data)
+    response = requests.post(f'{base_url}/', json={
+        'id': first_assignee_data.id,
+        'name':first_assignee_data.name,
+        'taskcount':first_assignee_data.taskcount
+    })
     assignee = response.json()
 
     assert response.status_code == 200
-    assert assignee['name'] == first_assignee_data['name']
+    assert assignee['name'] == first_assignee_data.name
 
-def test_create_assignee_first_repeat_error(
-        first_assignee_data: dict
-) -> None:
-    response = requests.post(f'{base_url}/', json=first_assignee_data)
-
-    assert response.status_code == 400
 
 def test_get_assignee_by_id(
-        first_assignee_data: dict
+        first_assignee_data: Assignee
 ) -> None:
-    created_assignee = requests.post(f'{base_url}/', json=first_assignee_data).json()
-    assignee_id = created_assignee['id']
+    retrieved_assignee = requests.get(f'{base_url}/{first_assignee_data.id}').json()
 
-    retrieved_assignee = requests.get(f'{base_url}/{assignee_id}').json()
+    assert retrieved_assignee['name'] == first_assignee_data.name
 
-    assert retrieved_assignee['name'] == first_assignee_data['name']
-
-def test_update_assignee_name(
-        first_assignee_data: dict
+def test_update_assignee(
+        first_assignee_data: Assignee
 ) -> None:
-    created_assignee = requests.post(f'{base_url}/', json=first_assignee_data).json()
-    assignee_id = created_assignee['id']
+    requests.put(f'{base_url}/{first_assignee_data.id}')
 
-    updated_assignee_data = {'name': 'Updated Assignee'}
-    requests.put(f'{base_url}/{assignee_id}', json=updated_assignee_data)
+    retrieved_assignee = requests.get(f'{base_url}/{first_assignee_data.id}').json()
 
-    retrieved_assignee = requests.get(f'{base_url}/{assignee_id}').json()
-
-    assert retrieved_assignee['name'] == updated_assignee_data['name']
-
-def test_delete_assignee(
-        first_assignee_data: dict
-) -> None:
-    created_assignee = requests.post(f'{base_url}/', json=first_assignee_data).json()
-    assignee_id = created_assignee['id']
-
-    requests.delete(f'{base_url}/{assignee_id}')
-
-    assert requests.get(f'{base_url}/{assignee_id}').status_code == 404
+    assert retrieved_assignee['taskcount'] == first_assignee_data.taskcount + 1
