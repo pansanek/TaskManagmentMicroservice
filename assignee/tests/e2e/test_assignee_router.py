e6062fb0fafc5e3435377a629ed0f8ed57449c14
Александр Потемкин <90202010+pansanek@users.noreply.github.com>
2023-12-08 00:30:47 +0300

    Update test_assignee_router.py

--- assignee/tests/e2e/test_assignee_router.py
+++ assignee/tests/e2e/test_assignee_router.py
@@ -1,66 +1,74 @@
+import time
 import pytest
-from fastapi.testclient import TestClient
-from app.main import app  # Assuming app is your FastAPI application
+import requests
+from uuid import UUID, uuid4
+from datetime import datetime
 
-# Assuming AssigneeService is part of your app
-from app.services.assignee_service import AssigneeService
+from app.models.assignee import Assignee
 
-client = TestClient(app)
-
+time.sleep(5)
+base_url = 'http://127.0.0.1:8000/api/assignees/'
 
 @pytest.fixture(scope='session')
-def assignee_data() -> dict:
+def first_assignee_data() -> dict:
     return {
-        'name': 'Jon Doe',
+        'name': 'Assignee 1',
     }
 
-
-def test_create_assignee_success(assignee_data: dict) -> None:
-    response = client.post("/api/assignee/", json=assignee_data)
-    assert response.status_code == 200
-    assert response.json()['name'] == assignee_data['name']
-
-
-def test_create_assignee_duplicate_error(assignee_data: dict) -> None:
-    # Assuming you have a mechanism to prevent duplicate names
-    client.post("/api/assignee/", json=assignee_data)
-
-    response = client.post("/api/assignee/", json=assignee_data)
-    assert response.status_code == 400
-
+@pytest.fixture(scope='session')
+def second_assignee_data() -> dict:
+    return {
+        'name': 'Assignee 2',
+    }
 
 def test_get_assignees_empty() -> None:
-    response = client.get("/api/assignee/")
+    assert requests.get(f'{base_url}/').json() == []
+
+def test_create_assignee_first_success(
+        first_assignee_data: dict
+) -> None:
+    response = requests.post(f'{base_url}/', json=first_assignee_data)
+    assignee = response.json()
+
     assert response.status_code == 200
-    assert response.json() == []
+    assert assignee['name'] == first_assignee_data['name']
 
+def test_create_assignee_first_repeat_error(
+        first_assignee_data: dict
+) -> None:
+    response = requests.post(f'{base_url}/', json=first_assignee_data)
 
-def test_get_assignees_non_empty(assignee_data: dict) -> None:
-    # Create an assignee first
-    client.post("/api/assignee/", json=assignee_data)
+    assert response.status_code == 400
 
-    response = client.get("/api/assignee/")
-    assert response.status_code == 200
-    assert len(response.json()) == 1
+def test_get_assignee_by_id(
+        first_assignee_data: dict
+) -> None:
+    created_assignee = requests.post(f'{base_url}/', json=first_assignee_data).json()
+    assignee_id = created_assignee['id']
 
+    retrieved_assignee = requests.get(f'{base_url}/{assignee_id}').json()
 
-def test_get_assignee_by_id_success(assignee_data: dict) -> None:
-    create_response = client.post("/api/assignee/", json=assignee_data)
-    assignee_id = create_response.json()['id']
+    assert retrieved_assignee['name'] == first_assignee_data['name']
 
-    response = client.get(f"/api/assignee/{assignee_id}")
-    assert response.status_code == 200
-    assert response.json()['id'] == assignee_id
+def test_update_assignee_name(
+        first_assignee_data: dict
+) -> None:
+    created_assignee = requests.post(f'{base_url}/', json=first_assignee_data).json()
+    assignee_id = created_assignee['id']
 
+    updated_assignee_data = {'name': 'Updated Assignee'}
+    requests.put(f'{base_url}/{assignee_id}', json=updated_assignee_data)
 
-def test_get_assignee_by_id_not_found() -> None:
-    response = client.get("/api/assignee/123")  # Assuming 123 is a non-existent ID
-    assert response.status_code == 404
+    retrieved_assignee = requests.get(f'{base_url}/{assignee_id}').json()
 
+    assert retrieved_assignee['name'] == updated_assignee_data['name']
 
-def test_update_assignee_success(assignee_data: dict) -> None:
-    create_response = client.post("/api/assignee/", json=assignee_data)
-    assignee_id = create_response.json()['id']
+def test_delete_assignee(
+        first_assignee_data: dict
+) -> None:
+    created_assignee = requests.post(f'{base_url}/', json=first_assignee_data).json()
+    assignee_id = created_assignee['id']
 
-    update_response = client.put(f"/api/assignee/{assignee_id}")
+    requests.delete(f'{base_url}/{assignee_id}')
 
+    assert requests.get(f'{base_url}/{assignee_id}').status_code == 404
