ab733cf30bfd47b54e4944cf014354922bf827e6
Александр Потемкин <90202010+pansanek@users.noreply.github.com>
2023-12-08 22:44:48 +0300

    Update test_assignee_router.py

--- assignee/tests/e2e/test_assignee_router.py
+++ assignee/tests/e2e/test_assignee_router.py
@@ -21,7 +21,7 @@ def first_assignee_data() -> Assignee:
 def second_assignee_data() -> Assignee:
     return Assignee(id=12,name='Namee',taskcount=0)
 
-def test_get_assignees_created_in_repo() -> None:
+def test_get_assignees_created_in_repo(base_url) -> None:
     time.sleep(5)
     assert requests.get(f'{base_url}/').json() == []
 
