b2df15b8346d06b4d87db6ff2ee12a6ef2d3f34b
Александр Потемкин <90202010+pansanek@users.noreply.github.com>
2023-12-08 12:57:07 +0300

    Update settings.py

--- task/app/settings.py
+++ task/app/settings.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    amqp_url: str = amqp://guest:guest@maprac6-rabbitmq-1:5672
+    amqp_url: str = "amqp://guest:guest@maprac6-rabbitmq-1:5672"
     postgres_url: str = "postgresql://postgres:potemkin@maprac6-postgres-1:5432/test"
 
     model_config = SettingsConfigDict(env_file='.env')
