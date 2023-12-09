6ea6c47257361930ac956c69ae74f2ea74c47dda
pansanek <alex.potjomkin@gmail.com>
2023-12-09 09:51:34 +0300

    integration tests

---
+++ task/tests/integration/test_task_repo.py
@@ -0,0 +1,58 @@
+import pytest
+from uuid import uuid4
+from datetime import datetime
+from app.models.task import Task, TaskStatuses
+from app.repositories.db_task_repo import TaskRepo
+
+@pytest.fixture()
+def task_repo() -> TaskRepo:
+    repo = TaskRepo()
+    return repo
+
+@pytest.fixture(scope='session')
+def first_task() -> Task:
+    return Task(
+        id=uuid4(),
+        title='Task 1',
+        description='Description for Task 1',
+        due_date=datetime.now(),
+        status=TaskStatuses.TODO,
+        assignee_id=1
+    )
+
+@pytest.fixture(scope='session')
+def second_task() -> Task:
+    return Task(
+        id=uuid4(),
+        title='Task 2',
+        description='Description for Task 2',
+        due_date=datetime.now(),
+        status=TaskStatuses.IN_PROGRESS,
+        assignee_id=2
+    )
+
+def test_empty_list(task_repo: TaskRepo) -> None:
+    assert task_repo.get_tasks() == []
+
+def test_create_task(task_repo: TaskRepo, first_task: Task) -> None:
+    created_task = task_repo.create_task(first_task)
+    assert created_task == first_task
+
+def test_get_task_by_id(task_repo: TaskRepo, first_task: Task) -> None:
+    task_repo.create_task(first_task)
+    retrieved_task = task_repo.get_task_by_id(first_task.id)
+    assert retrieved_task == first_task
+
+def test_set_status(task_repo: TaskRepo, first_task: Task) -> None:
+    task_repo.create_task(first_task)
+    new_status = TaskStatuses.IN_PROGRESS
+    first_task.status = new_status
+    updated_task = task_repo.set_status(first_task)
+    assert updated_task.status == new_status
+
+def test_start_task(task_repo: TaskRepo, first_task: Task) -> None:
+    task_repo.create_task(first_task)
+    new_status = TaskStatuses.IN_PROGRESS
+    first_task.status = new_status
+    started_task = task_repo.start_task(first_task)
+    assert started_task.status == new_status
