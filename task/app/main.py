7cc39d883b912e14ea61a1fa36e5d85b6b196373
pansanek <alex.potjomkin@gmail.com>
2023-12-10 17:08:11 +0300

    loki/promtail logging

--- task/app/main.py
+++ task/app/main.py
@@ -4,9 +4,17 @@ from fastapi import FastAPI
 
 from app import rabbitmq
 from app.endpoints.task_router import task_router,metrics_router
-
+import logging
+from logging_loki import LokiHandler
 app = FastAPI(title='Task Service')
 
+# loki_logs_handler = LokiHandler(
+#     url="http://loki:3100/loki/api/v1/push",
+#     tags={"application": "fastapi"},
+#     version="1",
+# )
+# logger = logging.getLogger("uvicorn.access")
+# logger.addHandler(loki_logs_handler)
 
 @app.on_event('startup')
 def startup():
