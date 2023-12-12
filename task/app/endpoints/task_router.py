15ace9550adf726d8ca83c2bdf176e49d1a6b1b9
pansanek <alex.potjomkin@gmail.com>
2023-12-12 15:37:03 +0300

    token invalid somehow :(

--- task/app/endpoints/task_router.py
+++ task/app/endpoints/task_router.py
@@ -22,16 +22,15 @@ oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="http://localhost:8080/realms/myrealm/protocol/openid-connect/token",
     scopes={"openid": "Read data with openid scope"})
 
-async def get_user_info(token: str = Depends(oauth2_scheme)):
-    url = "http://localhost:8080/realms/myrealm/protocol/openid-connect/userinfo"
-
-    payload = {}
+def get_user_info(token: str = Depends(oauth2_scheme)):
+    url = "http://maprac6-keycloak-1:8080/realms/myrealm/protocol/openid-connect/userinfo"
     headers = {
-        'Authorization': f'Bearer {token}'}
+        "Authorization": f"Bearer {token}"}
 
+    print(str(headers))
     try:
-        response = requests.request("GET", url, headers=headers, data=payload)
-        response.raise_for_status()  # Поднимаем исключение в случае ошибки HTTP
+        response = requests.request("GET", url, headers=headers)
+        print(f"Response: {response}")
         return response.json()
     except requests.RequestException as e:
         # Обработка ошибок запроса
@@ -40,34 +39,41 @@ async def get_user_info(token: str = Depends(oauth2_scheme)):
 
 # Example route that requires authentication
 @task_router.get("/secure-data")
-async def secure_data(current_user: dict = Depends(oauth2_scheme)):
+def secure_data(current_user: dict = Depends(oauth2_scheme)):
     return {"message": "You have access to secure data!", "user": current_user}
 
 
 @task_router.get('/')
-async def get_tasks(task_service: TaskService = Depends(TaskService),
+def get_tasks(task_service: TaskService = Depends(TaskService),
               current_user: dict = Depends(get_user_info)) -> list[Task]:
-    if "Viewer" not in current_user["realm_access"]["roles"]:
-        raise HTTPException(status_code=403, detail="Permission denied, user must have 'Viewer' role")
+    if "Viewer" or "Admin" not in current_user["realm_access"]["roles"]:
+        raise HTTPException(status_code=403, detail="Permission denied")
     return task_service.get_tasks()
 
 
 @task_router.post('/')
 def create_task(
         task_info: CreateTaskRequest,
-        task_service: TaskService = Depends(TaskService)
+        task_service: TaskService = Depends(TaskService),
+        current_user: dict = Depends(get_user_info)
 ) -> Task:
+    if "Admin" not in current_user["realm_access"]["roles"]:
+        raise HTTPException(status_code=403, detail="Permission denied")
     try:
         task = task_service.create_task(task_info.id, task_info.title, task_info.description, task_info.due_date,
                                         task_info.assignee_id)
-        asyncio.run(send_assignee_update_message(task_info.assignee_id))
+        # asyncio.run(send_assignee_update_message(task_info.assignee_id))
         return task.dict()
     except KeyError:
         raise HTTPException(400, f'Task with title={task_info.title} already exists')
 
 
 @task_router.post('/{id}/start')
-def start_task(id: UUID, task_service: TaskService = Depends(TaskService)) -> Task:
+def start_task(id: UUID, task_service: TaskService = Depends(TaskService),
+               current_user: dict = Depends(get_user_info)
+               ) -> Task:
+    if "Admin" not in current_user["realm_access"]["roles"]:
+        raise HTTPException(status_code=403, detail="Permission denied")
     try:
         task = task_service.start_task(id)
         return task.dict()
@@ -78,7 +84,11 @@ def start_task(id: UUID, task_service: TaskService = Depends(TaskService)) -> Ta
 
 
 @task_router.post('/{id}/complete')
-def complete_task(id: UUID, task_service: TaskService = Depends(TaskService)) -> Task:
+def complete_task(id: UUID, task_service: TaskService = Depends(TaskService),
+                  current_user: dict = Depends(get_user_info)
+                  ) -> Task:
+    if "Admin" not in current_user["realm_access"]["roles"]:
+        raise HTTPException(status_code=403, detail="Permission denied")
     try:
         task = task_service.complete_task(id)
         return task.dict()
@@ -89,7 +99,11 @@ def complete_task(id: UUID, task_service: TaskService = Depends(TaskService)) ->
 
 
 @task_router.post('/{id}/cancel')
-def cancel_task(id: UUID, task_service: TaskService = Depends(TaskService)) -> Task:
+def cancel_task(id: UUID, task_service: TaskService = Depends(TaskService),
+                current_user: dict = Depends(get_user_info)
+                ) -> Task:
+    if "Admin" not in current_user["realm_access"]["roles"]:
+        raise HTTPException(status_code=403, detail="Permission denied")
     try:
         task = task_service.cancel_task(id)
         return task.dict()
